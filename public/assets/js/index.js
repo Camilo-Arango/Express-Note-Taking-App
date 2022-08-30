6e04591645f441fde3f3a9650ce41b2ec03f89ce
Camilo Arango <camilo94@icloud.com>
2022-08-29 21:59:20 -0700

    more js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -100,3 +100,32 @@ var handleRenderSaveBtn = function () {
     $saveNoteBtn.show();
   }
 };
+
+// Render's the list of note titles
+var renderNoteList = function (notes) {
+  $noteList.empty();
+
+  var noteListItems = [];
+
+  for (var i = 0; i < notes.length; i++) {
+    var note = notes[i];
+
+    var $li = $("<li class='list-group-item'>").data(note);
+    var $span = $("<span>").text(note.title);
+    var $delBtn = $(
+      "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+    );
+
+    $li.append($span, $delBtn);
+    noteListItems.push($li);
+  }
+
+  $noteList.append(noteListItems);
+};
+
+// Gets notes from the db and renders them to the sidebar
+var getAndRenderNotes = function () {
+  return getNotes().then(function (data) {
+    renderNoteList(data);
+  });
+};
