2f15ddba5b217656155d4835a7f7506961a24885
Camilo Arango <camilo94@icloud.com>
2022-08-29 22:14:13 -0700

    finishing up the server.js file, time to test it

--- server.js
+++ server.js
@@ -38,3 +38,25 @@ app.post("/api/notes", function(req, res) {
     console.log("Note saved to db.json. Content: ", newNote);
     res.json(savedNotes);
 })
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(port, function() {
+    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+})
