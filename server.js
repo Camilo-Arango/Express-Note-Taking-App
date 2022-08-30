accbcda85e96153a3657e2e80ded59fab664935b
Camilo Arango <camilo94@icloud.com>
2022-08-29 22:11:40 -0700

    fixed filepath from having moved files to new folder

--- server.js
+++ server.js
@@ -4,25 +4,37 @@ const fs = require("fs");
 
 const app = express();
 const port = 8080;
-const mainDir = path.join(__dirname, "/public");
+const mainDir = path.join(__dirname, "./Develop/public");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(mainDir, "notes.html"));
+    res.sendFile(path.join(mainDir, "./Develop/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
